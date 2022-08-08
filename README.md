992d64f194491b936c28dd708bc0c304a69e108c
l0u0h0 <l0u0h0@gmail.com>
2022-08-08 18:19:30 +0900

    getter,setter

--- README.md
+++ README.md
@@ -1834,3 +1834,28 @@ class Person {
   constructor(public name: string, public age: number) {}
 }
 ```
+
+### Getters & Setters
+
+```ts
+class Person {
+  public constructor(private _name: string, private age: number) {}
+
+  // get을 하는 함수를 getter
+  // 읽기전용
+  get name() {
+    return this._name + " Lee";
+  }
+
+  // set을 하는 함수를 setter
+  // 쓰기전용
+  set name(n: string) {
+    this._name = n;
+  }
+}
+
+const p1: Person = new Person("Lee", 25);
+console.log(p1.name);
+p1.name = "Duck";
+console.log(p1.name);
+```
--- class/example.js
+++ class/example.js
@@ -7,11 +7,26 @@
 // }
 // const p1 = new Person("Lee");
 // console.log(p1);
+// class Person {
+//   constructor(public name: string, public age: number) {}
+// }
+// const p1: Person = new Person("Lee", 25);
+// console.log(p1);
 class Person {
-    constructor(name, age) {
-        this.name = name;
+    constructor(_name, age) {
+        this._name = _name;
         this.age = age;
+    }
+    get name() {
+        // console.log("get");
+        return this._name + " Lee";
+    }
+    set name(n) {
+        console.log("set");
+        this._name = n;
     }
 }
 const p1 = new Person("Lee", 25);
-console.log(p1);
+console.log(p1.name); // get을 하는 함수를 getter
+p1.name = "Duck"; // set을 하는 함수를 setter
+console.log(p1.name);
